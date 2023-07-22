793eb884def1f8bf72a0383dc90e84bb8ea4b8ed
Laur <laurentiu.vieru@it4best.com>
2023-07-22 10:52:49 +0300

    Working at implementing image api

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
 
    const headers = {
@@ -5,7 +7,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	} 
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
         headers: headers,
     });
 
@@ -29,3 +31,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   
     return rentalRatePerDay.toFixed(0);
 };
+
+
+export const generateCarImageUrl = (car: CarProps,angle?: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage/');
+
+    const { make, year, model } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+
+
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+}
